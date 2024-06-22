90b416de76a08c50b667149f30814c87bc68e82b
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2024-06-22 15:35:12 +0800

    try different ways to achive the encoding process

--- buffer/challenge.js
+++ buffer/challenge.js
@@ -12,3 +12,10 @@ memoryAllocation[2] = 0x21;
 
 console.log(memoryAllocation);
 console.log(memoryAllocation.toString('utf-8'));
+
+//another ways
+//from will much faster and allocate the extactly memory
+// const buffer = Buffer.from([0x48, 0x69, 0x21]);
+//with hex method
+const buffer = Buffer.from('486921', 'hex');
+console.log('buffer', buffer.toString('utf-8'));
