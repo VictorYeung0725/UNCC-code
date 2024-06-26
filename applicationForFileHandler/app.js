bcce0bfbb425efa2e37fe0d3af0c7b8adacb2453
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2024-06-26 22:00:02 +0800

    add delete rename and addcontent to file handler

--- applicationForFileHandler/app.js
+++ applicationForFileHandler/app.js
@@ -8,11 +8,16 @@ const { Buffer } = require('buffer');
 
 //here mainly use promise api // only use callback api if the performance is critical
 (async () => {
+  const CREATE_FILE = 'create a file';
+  const DELETE_FILE = 'delete a file';
+  const RENAME_FILE = 'rename a file';
+  const ADD_CONTENT_TO_FILE = 'add content to file';
+
   const createFile = async (path) => {
     try {
       //check whether or not file already exist
       const existingFileHandler = await fs.open(path, 'r');
-      //we do have the file
+      //close the file after checking
       existingFileHandler.close();
       return console.log(`the file ${path} already exist `);
     } catch (error) {
@@ -20,13 +25,25 @@ const { Buffer } = require('buffer');
       const newFileHandler = await fs.open(path, 'w');
       return console.log(`the file ${path} successfully created `);
     }
+  };
 
-    //close the file after checking
+  const deleteFile = async (path) => {
+    console.log(`Deleting ${path}`);
+    // try {
+    //   const existingFileHandler = await fs.open(path, 'r');
+    // } catch (error) {}
+  };
+
+  const renameFile = async (oldPath, newPath) => {
+    console.log(`Rename file from ${oldPath} to ${newPath}`);
+  };
+
+  const addToFile = async (path, content) => {
+    console.log(`Adding ${content} to this ${path}`);
   };
 
   const commandFileHandler = await fs.open('./command.txt', 'r');
   commandFileHandler.on('change', async () => {
-    const CREATE_FILE = 'create a file';
     //get the size of the file
     const size = (await commandFileHandler.stat()).size;
     //and allocate the buffer with the size of file
@@ -42,10 +59,37 @@ const { Buffer } = require('buffer');
     await commandFileHandler.read(buffer, offset, length, position);
     const command = buffer.toString('utf-8');
 
-    //create a file(path)
+    //create a file
+    //create a file<path>
     if (command.includes(CREATE_FILE)) {
       const filePath = command.substring(CREATE_FILE.length + 1);
       createFile(filePath);
+    }
+
+    //delete a file
+    //delete a file<path>
+    if (command.includes(DELETE_FILE)) {
+      const filePath = command.substring(DELETE_FILE + 1);
+      deleteFile(filePath);
+    }
+
+    //rename a file
+    //rename a file <path> to <newPath>
+    if (command.includes(RENAME_FILE)) {
+      const _idx = command.indexOf(' to ');
+      const oldPath = command.substring(RENAME_FILE + 1, _idx);
+      const newPath = command.substring(_idx + 4);
+
+      renameFile(oldPath, newPath);
+    }
+
+    //add to file
+    //add to file <path> this content:<content>
+    if (command.includes(ADD_CONTENT_TO_FILE)) {
+      const _idx = command.indexOf(' this content: ');
+      const filePath = command.substring(ADD_CONTENT_TO_FILE + 1, _idx);
+      const content = command.substring(_idx + 15);
+      addToFile(filePath, content);
     }
   });
 
