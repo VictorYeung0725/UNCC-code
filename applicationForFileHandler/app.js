ba32cf59157262e51d8652bd3c2390000c07d53b
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2024-06-26 20:45:31 +0800

    remove comment and add create file handler

--- applicationForFileHandler/app.js
+++ applicationForFileHandler/app.js
@@ -1,10 +1,6 @@
 //this application is able to edit read and rename and delete the file with node.js fs module
 const fs = require('fs/promises');
 const { Buffer } = require('buffer');
-//three ways for read file
-//1. promise api
-//2. callback api
-//3. synchrnos api
 
 //first step is to open the file (file discriptor) just a number that represent the file in memory
 // then can read the file
@@ -12,12 +8,25 @@ const { Buffer } = require('buffer');
 
 //here mainly use promise api // only use callback api if the performance is critical
 (async () => {
+  const createFile = async (path) => {
+    try {
+      //check whether or not file already exist
+      const existingFileHandler = await fs.open(path, 'r');
+      //we do have the file
+      existingFileHandler.close();
+      return console.log(`the file ${path} already exist `);
+    } catch (error) {
+      //if error, we dont have the file, now we create it
+      const newFileHandler = await fs.open(path, 'w');
+      return console.log(`the file ${path} successfully created `);
+    }
+
+    //close the file after checking
+  };
+
   const commandFileHandler = await fs.open('./command.txt', 'r');
-
-  //restructure the code with event emitter
   commandFileHandler.on('change', async () => {
-    console.log('the file is changed');
-
+    const CREATE_FILE = 'create a file';
     //get the size of the file
     const size = (await commandFileHandler.stat()).size;
     //and allocate the buffer with the size of file
@@ -31,43 +40,18 @@ const { Buffer } = require('buffer');
 
     //we want to read the whole content (from beginning till the end)
     await commandFileHandler.read(buffer, offset, length, position);
-    console.log(buffer.toString('utf-8'));
     const command = buffer.toString('utf-8');
 
-    //create a file
     //create a file(path)
-    if (command.includes('create a file')) {
-      //do something
+    if (command.includes(CREATE_FILE)) {
+      const filePath = command.substring(CREATE_FILE.length + 1);
+      createFile(filePath);
     }
   });
 
   const watcher = fs.watch('./command.txt');
   //here we watch the file changed type to monitor the change
   for await (const event of watcher) {
-    console.log(event.eventType);
     commandFileHandler.emit('change');
-    // if (event.eventType === 'change') {
-    //   console.log(event, 'the file is changed');
-
-    //   //get the size of the file
-    //   const size = (await commandFileHandler.stat()).size;
-    //   //and allocate the buffer with the size of file
-    //   const buff = Buffer.alloc(size);
-    //   //the location which we want to filter the position
-    //   const offset = 0;
-    //   //how many byte to read
-    //   const length = buff.byteLength;
-    //   //the position where start to read the file from
-    //   const position = 0;
-
-    //   //we want to read the whole content (from beginning till the end)
-    //   const content = await commandFileHandler.read(
-    //     buff,
-    //     offset,
-    //     length,
-    //     position
-    //   );
-    //   console.log(content);
-    // }
   }
 })();
